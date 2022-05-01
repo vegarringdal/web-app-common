2e83c70317dafa7c955b3bf03350ab76a730a8b8
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-05-01 15:21:50 +0200

    chore: update webuserrole api

--- src/default_api_config/aiWebUserRoleApi.ts
+++ src/default_api_config/aiWebUserRoleApi.ts
@@ -13,17 +13,36 @@ export const aiWebUserRoleApi: ApiInterface = {
         {
             name: "WEB_USER_ID",
             type: "number",
-            hiddenByDefault: true
-        },
-        {
-            name: "WEB_ROLE_ID",
-            type: "number",
-            hiddenByDefault: true
+            accessUpdate: [],
+            parentColumnsFromTo: [["_", "USERNAME"]]
         },
         {
             name: "NAME",
             label: "ROLE NAME",
-            type: "text"
+            type: "text",
+            // RELATED
+            parentViewApi: "WEB_USER",
+            parentTitle: "Select user",
+            parentFrom: "ID",
+            parentTo: "WEB_USER_ID",
+            parentColumnsFromTo: [["USERNAME", "USERNAME"]]
+        },
+        {
+            name: "WEB_ROLE_ID",
+            type: "number",
+            accessUpdate: [],
+            parentColumnsFromTo: [["_", "CABLETYPE"]]
+        },
+        {
+            name: "NAME",
+            label: "ROLE NAME",
+            type: "text",
+            // RELATED
+            parentViewApi: "WEB_ROLE",
+            parentTitle: "Select role",
+            parentFrom: "ID",
+            parentTo: "WEB_ROLE_ID",
+            parentColumnsFromTo: [["NAME", "NAME"]]
         },
         {
             name: "MODIFIED",
